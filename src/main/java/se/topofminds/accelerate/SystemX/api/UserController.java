2fbdf6e7a104d3951f3762a7adef022cc759c8a3
Jerry <jerry.engstrom@topofminds.se>
2019-10-18 14:43:57 +0200

    Fixed save user

--- src/main/java/se/topofminds/accelerate/SystemX/api/UserController.java
+++ src/main/java/se/topofminds/accelerate/SystemX/api/UserController.java
@@ -4,8 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import se.topofminds.accelerate.SystemX.entity.Privilege;
 import se.topofminds.accelerate.SystemX.entity.User;
+import se.topofminds.accelerate.SystemX.model.UserModel;
 import se.topofminds.accelerate.SystemX.repository.UserRepo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
 
 @RestController
 public class UserController {
@@ -13,7 +21,24 @@ public class UserController {
     UserRepo repo;
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        repo.save(user);
+    public void createUser(@RequestBody UserModel user) throws Exception {
+        User u = repo.findByUsername(user.getUsername());
+        if(u != null){
+            throw new Exception("User allready exists");
+        }
+        long id = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        u = new User();
+        u.setId(id);
+        u.setPassword(user.getPassword());
+        u.setUsername(user.getUsername());
+        List<Privilege> privs = new ArrayList<Privilege>();
+        for(String p : user.getPrivileges()) {
+            Privilege pr = new Privilege();
+            pr.setUserId(id);
+            pr.setPrivilege(p);
+            privs.add(pr);
+        }
+        u.setPrivileges(privs);
+        repo.save(u);
     }
 }
---
+++ src/main/java/se/topofminds/accelerate/SystemX/model/UserModel.java
@@ -0,0 +1,33 @@
+package se.topofminds.accelerate.SystemX.model;
+
+import java.util.List;
+
+public class UserModel {
+    private String username;
+    private String password;
+    private List<String> privileges;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getPrivileges() {
+        return privileges;
+    }
+
+    public void setPrivileges(List<String> privileges) {
+        this.privileges = privileges;
+    }
+}
