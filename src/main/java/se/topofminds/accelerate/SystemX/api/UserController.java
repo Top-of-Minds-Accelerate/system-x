efed0375667f6b2710c186ce5abd45d7d071d66d
Olle Hagner <olle.hagner@choice.se>
2019-10-18 15:14:55 +0200

    Update privileges and list all users

--- src/main/java/se/topofminds/accelerate/SystemX/api/UserController.java
+++ src/main/java/se/topofminds/accelerate/SystemX/api/UserController.java
@@ -1,9 +1,7 @@
 package se.topofminds.accelerate.SystemX.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import se.topofminds.accelerate.SystemX.entity.Privilege;
 import se.topofminds.accelerate.SystemX.entity.User;
@@ -14,6 +12,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
@@ -23,8 +22,8 @@ public class UserController {
     @PostMapping("/users")
     public void createUser(@RequestBody UserModel user) throws Exception {
         User u = repo.findByUsername(user.getUsername());
-        if(u != null){
-            throw new Exception("User allready exists");
+        if (u != null) {
+            throw new Exception("User already exists");
         }
         long id = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
         u = new User();
@@ -32,7 +31,7 @@ public class UserController {
         u.setPassword(user.getPassword());
         u.setUsername(user.getUsername());
         List<Privilege> privs = new ArrayList<Privilege>();
-        for(String p : user.getPrivileges()) {
+        for (String p : user.getPrivileges()) {
             Privilege pr = new Privilege();
             pr.setUserId(id);
             pr.setPrivilege(p);
@@ -40,5 +39,31 @@ public class UserController {
         }
         u.setPrivileges(privs);
         repo.save(u);
+    }
+
+    @GetMapping("/users")
+    public List<UserModel> listUsers() throws Exception {
+        List<UserModel> users = new ArrayList<>();
+        repo.findAll().forEach(entity -> users.add(UserModel.fromUser(entity)));
+        return users;
+    }
+
+    @PutMapping("/users/{username}/privileges")
+    public void updateUser(@RequestBody List<String> privileges, @PathVariable String username) throws Exception {
+        User user = repo.findByUsername(username);
+        if (user == null) {
+            throw new Exception("User doesn't exist");
+        }
+        List<Privilege> updatedPrivileges = privileges.stream()
+                .map((String privilegeString) -> {
+                    Privilege privilege = new Privilege();
+                    privilege.setPrivilege(privilegeString);
+                    privilege.setUserId(user.getId());
+                    return privilege;
+                })
+                .collect(Collectors.toList());
+        user.setPrivileges(updatedPrivileges);
+        repo.save(user);
+
     }
 }
--- src/main/java/se/topofminds/accelerate/SystemX/model/UserModel.java
+++ src/main/java/se/topofminds/accelerate/SystemX/model/UserModel.java
@@ -1,8 +1,24 @@
 package se.topofminds.accelerate.SystemX.model;
 
+import se.topofminds.accelerate.SystemX.entity.Privilege;
+import se.topofminds.accelerate.SystemX.entity.User;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserModel {
+
+    public static UserModel fromUser(User entity) {
+        UserModel model = new UserModel();
+        model.setUsername(entity.getUsername());
+        model.setPassword(entity.getPassword());
+        model.setPrivileges(
+                entity.getPrivileges().stream()
+                .map(Privilege::getPrivilege)
+                .collect(Collectors.toList())
+                );
+        return model;
+    }
     private String username;
     private String password;
     private List<String> privileges;
